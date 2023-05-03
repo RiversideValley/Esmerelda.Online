697dc78cbe0714efb6c2b6eeef6c789d0859a9a8
Lamparter <git@theriversi.de>
2023-05-03 08:01:09 +0100

    ✨ Some additions

---
+++ src/lib/NavBar/NavBar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { TextBlock } from "fluent-svelte";
+
+	interface Item {
+		href: string;
+		name: string;
+		icon?: string;
+	}
+
+	export let items: Item[];
+</script>
+
+<header class="navbar">
+	<div class="navbar-inner">
+		<a class="logo" href="/">
+			<img src="https://cloud.esmerelda.tech/Branding/Assets/Esmerelda/Logo.png" width="32" height="32" alt="Esmerelda Logo" />
+			Esmerelda <TextBlock variant="caption">dev</TextBlock>
+		</a>
+		<nav>
+			{#each items as { href, name, icon }}
+				<a
+					{href}
+					sveltekit:prefetch
+					class:selected={$page.url.pathname === href ||
+						($page.url.pathname.split("/").length > 1 &&
+							href.split("/").length > 1 &&
+							$page.url.pathname.startsWith(href) &&
+							!(href === "" || href === "/")) ||
+						(href === "/" && $page.url.pathname === "")}
+				>
+					{#if icon}
+						{@html icon}
+					{/if}
+					<span>{name}</span>
+				</a>
+			{/each}
+		</nav>
+		<div class="buttons">
+			<slot name="buttons" />
+		</div>
+	</div>
+</header>
+
+<style lang="scss">
+	@use "./NavBar";
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { NavBar } from "$lib";
+    import { onMount } from "svelte";
+    import { ProgressRing, Button, ContentDialog, TextBlock } from "fluent-svelte";
+
+	import Alert from "@fluentui/svg-icons/icons/alert_urgent_24_filled.svg?raw"
+
+    	const items = [
+        {
+			name: "{UserName}",
+			href: "/_federal/account",
+			content: 1,
+			icon: ""
+		},
+		{
+			name: "",
+			href: "/_notifications",
+			content: 1,
+			icon: Alert
+		}
+	];
+
+    let theme: "light" | "dark" = "light";
+
+	onMount(() => {
+		theme = window?.matchMedia("(prefers-color-scheme: dark)")?.matches ? "dark" : "light";
+
+		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => {
+			theme = e.matches ? "dark" : "light";
+		});
+	});
+</script>
+
+<NavBar {items} />
+
