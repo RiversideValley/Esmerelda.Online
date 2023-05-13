650917d00db69120894b94cfdd444374116ef35d
Lamparter <lamparter@theriversi.de>
2023-05-13 10:49:57 +0100

    🐛 Oops forgot https protocol

--- src/esme/streamliner.ts
+++ src/esme/streamliner.ts
@@ -1,7 +1,7 @@
 export const config = {
     msal: {
         endpoint: {
-            rel: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ded5e9d5-eb0c-45c4-a26f-3d665262f8a1&scope=user.read%20mail.read&response_type=code&redirect_uri=http://www.esmerelda.tech/_federal/callback",
+            rel: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ded5e9d5-eb0c-45c4-a26f-3d665262f8a1&scope=user.read%20mail.read&response_type=code&redirect_uri=https://www.esmerelda.tech/_federal/callback",
             dev: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ded5e9d5-eb0c-45c4-a26f-3d665262f8a1&scope=user.read%20mail.read&response_type=code&redirect_uri=http://localhost:5173/_federal/callback",
             pre: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ded5e9d5-eb0c-45c4-a26f-3d665262f8a1&scope=user.read%20mail.read&response_type=code&redirect_uri=http://localhost:4173/_federal/callback"
         },
